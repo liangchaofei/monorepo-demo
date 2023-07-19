25c1e0d8ffda68d723fae6f8b3729f67557b3cc0
liangchaofei <liangchaofei@liangchaofeideMacBook-Pro.local>
2023-07-19 17:30:38 +0800

    feat: init

---
+++ packages/pkg-b/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// packages/pkg-b/index.ts
+var pkg_a_1 = require("@alijk/pkg-a");
+console.log((0, pkg_a_1.sum)(1, 2));
---
+++ packages/pkg-b/index.ts
@@ -0,0 +1,4 @@
+// packages/pkg-b/index.ts
+import { sum } from '@alijk/pkg-a'
+
+console.log(sum(1, 2))
